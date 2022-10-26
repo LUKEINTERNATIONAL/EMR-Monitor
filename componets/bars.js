ed7c93af97c61ccb0251e1cb64376f8f4e22ac60
petroskayange <kayangepetros@gmail.com>
2022-10-26 09:53:45 +0200

    modfiying facility, user, and dashboard

---
+++ componets/bars.js
@@ -0,0 +1,188 @@
+class Bars extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `
+        <!-- Preloader -->
+        <div class="preloader flex-column justify-content-center align-items-center">
+          <img class="animation__shake" src="/dist/img/AdminLTELogo.png" alt="EMR MONITOR" height="60" width="60">
+        </div>
+      
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+          <!-- Left navbar links -->
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+              <a href="/index.html" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+              <a href="#" class="nav-link">Contact</a>
+            </li>
+          </ul>
+      
+          <!-- Right navbar links -->
+          <ul class="navbar-nav ml-auto">
+            <!-- Navbar Search -->
+            <li class="nav-item">
+              <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+                <i class="fas fa-search"></i>
+              </a>
+              <div class="navbar-search-block">
+                <form class="form-inline">
+                  <div class="input-group input-group-sm">
+                    <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                    <div class="input-group-append">
+                      <button class="btn btn-navbar" type="submit">
+                        <i class="fas fa-search"></i>
+                      </button>
+                      <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                        <i class="fas fa-times"></i>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </li>
+      
+            <!-- Notifications Dropdown Menu -->
+            <li class="nav-item dropdown">
+              <a class="nav-link" data-toggle="dropdown" href="#">
+                <i class="far fa-bell"></i>
+                <span class="badge badge-warning navbar-badge">15</span>
+              </a>
+              <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <span class="dropdown-item dropdown-header">15 Notifications</span>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">
+                  <i class="fas fa-envelope mr-2"></i> 4 new messages
+                  <span class="float-right text-muted text-sm">3 mins</span>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">
+                  <i class="fas fa-users mr-2"></i> 8 friend requests
+                  <span class="float-right text-muted text-sm">12 hours</span>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">
+                  <i class="fas fa-file mr-2"></i> 3 new reports
+                  <span class="float-right text-muted text-sm">2 days</span>
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                <i class="fas fa-expand-arrows-alt"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
+                <i class="fas fa-th-large"></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <!-- /.navbar -->
+      
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+          <!-- Brand Logo -->
+          <a href="/index.html" class="brand-link">
+            <img src="/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <span class="brand-text font-weight-light">EMR MONITOR</span>
+          </a>
+      
+          <!-- Sidebar -->
+          <div class="sidebar">
+            <!-- Sidebar user panel (optional) -->
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+              <div class="image">
+                <img src="/dist/img/avatar5.png" class="img-circle elevation-2" alt="User Image">
+              </div>
+              <div class="info">
+                <a href="#" class="d-block">Petros Kayange</a>
+              </div>
+            </div>
+      
+            <!-- SidebarSearch Form -->
+            <div class="form-inline">
+              <div class="input-group" data-widget="sidebar-search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                  <button class="btn btn-sidebar">
+                    <i class="fas fa-search fa-fw"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+      
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+              <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+                     with font-awesome or any other icon font library -->
+                <li class="nav-item menu-open">
+                  <a href="/index.html" class="nav-link">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                      Dashboard
+                    </p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-edit"></i>
+                    <p>
+                      General
+                      <i class="fas fa-angle-left right"></i>
+                    </p>
+                  </a>
+                  <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                      <a href="/pages/forms/manage_facilities.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Manage Facilities</p>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="/pages/forms/manage_users.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Manage Users</p>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="/pages/forms/editors.html" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Reports</p>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+             
+              </ul>
+            </nav>
+            <!-- /.sidebar-menu -->
+          </div>
+          <!-- /.sidebar -->
+        </aside>
+        `
+    }
+}
+
+class Footer extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `
+        <footer class="main-footer">
+            <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">EMR MONITOR</a>.</strong>
+            All rights reserved.
+            <div class="float-right d-none d-sm-inline-block">
+            <b>Version</b> 3.2.0
+            </div>
+        </footer>
+        `
+    }
+}
+customElements.define('app-footer',Footer)
+customElements.define('app-bars',Bars)
---
+++ dist/js/backend.js
@@ -0,0 +1,174 @@
+function submitParameters(parameters, url, returnToFunction) {
+ loader();
+    var parametersPassed = JSON.stringify(parameters);
+    
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && (this.status == 201 || this.status == 200)) {
+        dismissLoader()
+        var obj = JSON.parse(this.responseText);
+        eval(returnToFunction)(obj);
+      
+      }
+      dismissLoader()
+    };
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader('Authorization', sessionStorage.getItem("authorization"));
+    xhttp.setRequestHeader('Content-type', "application/json");
+    xhttp.send(parametersPassed);
+  }
+  
+  
+  function getData(url,returnToFunction) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      console.log(this.status)
+      if (this.readyState == 4 && (this.status == 201 || this.status == 200)) {
+        dismissLoader()
+        var obj = JSON.parse(this.responseText);
+        eval(returnToFunction)(obj);
+      }
+      dismissLoader()
+    };
+    xhttp.open("GET", url, true);
+    xhttp.setRequestHeader('Authorization', sessionStorage.getItem("authorization"));
+    xhttp.setRequestHeader('Content-type', "application/json");
+    xhttp.send();
+  }
+            
+
+function submitFormData(url){
+  $("form").on( "submit", function(e) {
+    loader();
+  var dataString = $(this).serialize();
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: dataString,
+    dataType:'JSON',
+    success: function (data) {
+      console.log(data)
+      dismissLoader();
+      if(data == "login successfully")
+       window.location = "/php_api/views/dashboard.php";
+      Toast.fire({
+        icon: 'success',
+        title: 'Successfully submitted'
+      })
+      setTimeout(() => { window.location.reload().delay; }, 1000);
+      
+    },
+    error: function (error) {
+      
+      if(error.statusText)
+       var title = error.statusText;
+      else
+       var title = 'Failing to submit';
+      Toast.fire({
+      icon: 'error',
+      title: title
+    })
+    dismissLoader();
+    }
+  });
+
+  e.preventDefault();
+  });
+}
+function submitLogin(url){
+  $("form").on( "submit", function(e) {
+    loader();
+  var dataString = $(this).serialize();
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: dataString,
+    dataType:'JSON',
+    success: function (data) {
+      console.log(data)
+      dismissLoader();
+      Toast.fire({
+        icon: 'success',
+        title: 'Successfully submitted'
+      })
+      window.location = "/php_api/index.php";
+    },
+    error: function (error) {
+      if(error.statusText)
+       var title = error.statusText;
+      else
+       var title = 'Failing to submit';
+      Toast.fire({
+      icon: 'error',
+      title: title
+    })
+    dismissLoader();
+    }
+  });
+
+  e.preventDefault();
+  });
+}
+function deleteData(url){
+  loader();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && (this.status == 201 || this.status == 200)) {
+      dismissLoader()
+      Toast.fire({
+        icon: 'success',
+        title: 'Successfully Deleted'
+      })
+      setTimeout(() => { window.location.reload().delay; }, 1000);
+    }
+    dismissLoader()
+  };
+  xhttp.open("GET", url, true);
+  xhttp.setRequestHeader('Authorization', sessionStorage.getItem("authorization"));
+  xhttp.setRequestHeader('Content-type', "application/json");
+  xhttp.send();
+}
+
+function setCookie(name,value,days) {
+  var expires = "";
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+function eraseCookie(name) {   
+  document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function loader(){
+  console.log("lodderrr")
+  try {
+    $('body').append(
+      `
+      <div class="cover_page" style="width:100%; height:100%; background:#000;position: absolute; top: 0;opacity: 0.5;">
+        <img src="${base_url}views/dist/img/formloader.gif" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" 
+        style="opacity: .8;position: absolute; margin: auto; top: 0; left: 0; right: 0; bottom: 0;">
+      </div>
+      `);
+  } catch (error) {
+    console.log(error)
+  }
+ 
+}
+
+function dismissLoader(){
+  console.log("disssimmm")
+  // $('.cover_page').attr('style','display:none')
+}
