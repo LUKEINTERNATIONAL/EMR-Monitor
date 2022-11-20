30d0bdc1601c14591471e8d02acb188cf0d36770
petroskayange <kayangepetros@gmail.com>
2022-11-20 06:27:52 +0200

    addition of patient data and loader

--- dist/js/backend.js
+++ dist/js/backend.js
@@ -1,9 +1,10 @@
 function submitParameters(parameters, url, returnToFunction) {
+  loader_up()
   var parametersPassed = JSON.stringify(parameters);
-  
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && (this.status == 201 || this.status == 200)) {
+      loader_down()
       alert("Saved Successfully")
       window.location.reload()
       // try {
@@ -25,9 +26,11 @@ function submitParameters(parameters, url, returnToFunction) {
   
   
 function getData(url,returnToFunction) {
+  loader_up()
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && (this.status == 201 || this.status == 200)) {
+      loader_down()
       var obj = JSON.parse(this.responseText);
       eval(returnToFunction)(obj);
     }else if(this.status == 401){
@@ -41,8 +44,10 @@ function getData(url,returnToFunction) {
 }
 
 function deleteData(url) {
+  loader_up()
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
+    loader_down()
     if (this.readyState == 4 && (this.status == 201 || this.status == 200)) {
      alert("Successfully deleted")
      window.location.reload()
@@ -57,9 +62,11 @@ function deleteData(url) {
 }
 
 function updateData(url) {
+  loader_up()
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && (this.status == 201 || this.status == 200)) {
+      loader_down()
       alert("Successfully Updated")
       window.location.reload()
     }else if(this.status == 401){
@@ -97,18 +104,161 @@ function eraseCookie(name) {
   document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 
-function loader() {
-  // console.log("lodderrr")
-  try {
-    $('body').append(
-      `
-      <div class="cover_page" style="width:100%; height:100%; background:#000;position: absolute; top: 0;opacity: 0.5;">
-        <img src="${base_url}/views/dist/img/formloader.gif" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" 
-        style="opacity: .8;position: absolute; margin: auto; top: 0; left: 0; right: 0; bottom: 0;">
-      </div>
-      `);
-  } catch (error) {
-    console.log(error)
-  }
+function loader_up() {
+  $('#loader-3').attr('style','')
+  $('#cover').attr('style','')
 }
+function loader_down() {
+  $('#loader-3').attr('style','display:none')
+  $('#cover').attr('style','display:none')
+}
+
+style=`
+<style>
+/*Horizontal circles as bars loader-3 */
+#cover{
+  background: black;
+width: 100%;
+position: absolute;
+z-index: 1000;
+top: 0px;
+hieght: 1000px;
+height: 100%;
+opacity: 0.5;
+}
+#loader-3 #loader{
+	position: absolute;
+	width: 2vw;
+	height: 2vw;
+	background: rgb(255,0,0);
+	top:50%;
+	left: 50%;
+	border-radius: 50%;
+    z-index: 1500;
+    -webkit-animation: forward 2.3s linear infinite;
+    -moz-animation: forward 2.3s linear infinite;
+    -o-animation: forward 2.3s linear infinite;
+    animation: forward 2.3s linear infinite ;
+}
+
+#loader-3 > #loader:nth-of-type(1) {
+	-webkit-animation-delay: -0.46s;
+	-moz-animation-delay: -0.46s;
+	-o-animation-delay: -0.46s;
+		animation-delay: -0.46s;
+}
+
+#loader-3 > #loader:nth-of-type(2) {
+	-webkit-animation-delay: -0.92s;
+	-moz-animation-delay: -0.92s;
+	-o-animation-delay: -0.92s;
+		animation-delay: -0.92s;
+}
+#loader-3 > #loader:nth-of-type(3) {
+	-webkit-animation-delay: -1.38s;
+	-moz-animation-delay: -1.38s;
+	-o-animation-delay: -1.38s;
+		animation-delay: -1.38s;
+}
+#loader-3 > #loader:nth-of-type(4) {
+	-webkit-animation-delay: -1.84s;
+	-moz-animation-delay: -1.84s;
+	-o-animation-delay: -1.84s;
+		animation-delay: -1.84s;
+}
+
+/*keyframes for forward animations*/
+
+@-webkit-keyframes forward {
+	0% {
+		left: 40%;
+		opacity: 0;
+		background: rgb(255,255,0);
+	}
+	10% {
+		left: 45%;
+		opacity: 1;
+	}
+	90% {
+		left: 55%;
+		opacity: 1;
+	}
+	100% {
+		left: 62%;
+		opacity: 0;
+	}
+}
+
+@-moz-keyframes forward {
+	0% {
+		left: 40%;
+		opacity: 0;
+		background: rgb(255,255,0);
+	}
+	10% {
+		left: 45%;
+		opacity: 1;
+	}
+	90% {
+		left: 55%;
+		opacity: 1;
+	}
+	100% {
+		left: 62%;
+		opacity: 0;
+	}
+}
+
+@-o-keyframes forward {
+	0% {
+		left: 40%;
+		opacity: 0;
+		background: rgb(255,255,0);
+	}
+	10% {
+		left: 45%;
+		opacity: 1;
+	}
+	90% {
+		left: 55%;
+		opacity: 1;
+	}
+	100% {
+		left: 62%;
+		opacity: 0;
+	}
+}
+
+@keyframes forward {
+	0% {
+		left: 40%;
+		opacity: 0;
+		background: rgb(255,255,0);
+	}
+	10% {
+		left: 45%;
+		opacity: 1;
+	}
+	90% {
+		left: 55%;
+		opacity: 1;
+	}
+	100% {
+		left: 62%;
+		opacity: 0;
+	}
+}
+</style>
+`
+ll = `
+<div id="cover" style="display:none"></div>
+<div class="loader-wrapper" id="loader-3" style="display:none">
+  <div id="loader"></div>
+  <div id="loader"></div>
+  <div id="loader"></div>
+  <div id="loader"></div>
+  <div id="loader"></div>
+</div>`
+$('body').append(style)
+$('body').append(ll)
 
